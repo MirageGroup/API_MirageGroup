7cac1585bc0464fee21efcc9f0178c464ea2302a
Pedro <pedro11pucci@gmail.com>
2022-08-31 20:15:31 -0300

    initial files

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config.update(
+  TEMPLATES_AUTO_RELOAD = True
+)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run()
