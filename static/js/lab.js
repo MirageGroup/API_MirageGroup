f64e5e52b82ba13f7e9593929439fe252ca37f6f
gustavohpereira <gustavohpa2003@gmail.com>
2022-09-28 21:09:12 -0300

    add login and add popover

--- static/js/lab.js
+++ static/js/lab.js
@@ -30,6 +30,10 @@ function changeContainerClass(number, situation, description) {
 }
 
 
-  // drag and drop
+  // popover
+
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 
   
