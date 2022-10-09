454e811d9c64e35b689f1e42a60d43e0463739ff
pedro11pucci <pedro11pucci@gmail.com>
2022-10-09 16:30:24 -0300

    o chamado muda a cor do computador no mapa do laboratório

--- db.py
+++ db.py
@@ -29,11 +29,18 @@ def retrieveCalls():
     return chamados
 
 def createCall(form, labnum):
+    cursor = mysql.connection.cursor()
     pc_id = form.input_numero_pc.data
     pc_problem = form.pc_problem.data
     problem_description = form.problem_description.data
     labnum = str(labnum)
-    cursor = mysql.connection.cursor()
+    updatePcStatus(labnum, pc_id, pc_problem, problem_description)
     cursor.execute(f''' INSERT INTO chamados (laboratorio_num, pc_id, data_chamado, hora_chamado, problema_tipo, problema_desc) VALUES (%s, %s, CURDATE(), CURRENT_TIME(), %s, %s) ''', (labnum, pc_id, pc_problem, problem_description))
+    mysql.connection.commit()
+    cursor.close()
+
+def updatePcStatus(labnum, pc_id, pc_problem, problem_description):
+    cursor = mysql.connection.cursor()
+    cursor.execute(f''' UPDATE laboratorio{labnum} SET pc_problema = %s, pc_descricao = %s WHERE pc_id = %s''', (pc_problem, problem_description, pc_id))
     mysql.connection.commit()
     cursor.close()
