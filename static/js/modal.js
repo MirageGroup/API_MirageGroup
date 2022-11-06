c0f9d100d5ee4432a74eff170690cc8769297585
pedro11pucci <pedro11pucci@gmail.com>
2022-11-06 14:42:39 -0300

    informações do chamado aparecem no modal do layout

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,5 +1,5 @@
 const exampleModal = document.getElementById('modal-teste')
-    exampleModal.addEventListener('show.bs.modal', event => {
+exampleModal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
     const button = event.relatedTarget
     // Extract info from data-bs-* attributes
@@ -11,9 +11,48 @@ const exampleModal = document.getElementById('modal-teste')
     
     const modalBodyInput = exampleModal.querySelector('#input_numero_pc')
     
-    
     modalBodyInput.value = recipient
     })
+
+
+const modalRed = document.getElementById('modal-red')
+modalRed.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-problema')
+    const obj = JSON.parse(recipient);
+
+    pc_problem = obj.pc_problema
+    pc_description = obj.pc_descricao
+
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+
+    const modalInputProblem = modalRed.querySelector('#problema')
+    const modalInputDescription = modalRed.querySelector('#descricao')
+
+    modalInputProblem.value = pc_problem
+    modalInputDescription.value = pc_description
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
