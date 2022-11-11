2d913f6cfdd158f65d5175efef9816926ab7041b
gustavohpereira <gustavohpa2003@gmail.com>
2022-11-11 16:15:26 -0300

    :construction: feat create tabels in estatisticas

--- static/js/estatisticas.js
+++ static/js/estatisticas.js
@@ -27,3 +27,32 @@ const labels = [
         document.getElementById('myChart'),
         config
       );
+
+
+      const dataReports = {
+        labels: [
+          'Red',
+          'Blue',
+          
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, ],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            
+          ],
+          hoverOffset: 4
+        }]
+      };
+
+      const configReports = {
+        type: 'doughnut',
+        data: dataReports,
+      };
+    
+      const myChartReports = new Chart(
+        document.getElementById('myChartReports'),
+        configReports
+      );
